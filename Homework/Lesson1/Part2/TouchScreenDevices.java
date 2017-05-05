cb25ee403a4a74253844654213f907ba590345d5
Jane Geryak <janegeryak@gmail.com>
2017-05-05 16:37:52 +0300

    TouchScreenDevices

---
+++ Homework/Lesson1/Part2/TouchScreenDevices.java
@@ -0,0 +1,23 @@
+public class TouchScreenDevices {
+    public String name;
+    public String model;
+    private String type;
+    public int price;
+    public long date_of_manufacture;
+    private int size;
+    private int detuned;
+    public int increased_screen_sensitivity;
+    public int working_device;
+    public int broken_device;
+
+    public float getPrice (){};
+    private boolean isPhone (){};
+    private boolean isTablet (){};
+    public String getName (){};
+    public String getModel(){};
+    public int getBrand(){};
+    public long date_of_manufacture(){};
+    private int getIncreasedScreenSensitivity(){};
+    private setScreenSize(new_size) {
+        size = new_size;}
+}
