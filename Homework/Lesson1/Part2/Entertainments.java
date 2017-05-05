13b8d0564c913da4944b41d0b2ad76006801b042
Jane Geryak <janegeryak@gmail.com>
2017-05-05 15:54:27 +0300

    class Entertainment.java

---
+++ Homework/Lesson1/Part2/Entertainments.java
@@ -0,0 +1,24 @@
+public class Entertainments {
+    public String name;
+    public String place;
+    public int date;
+    public int active_entertainment;
+    public int passive_entertainment
+    public int quantity;
+    public int price;
+    public long time;
+
+    public float getPrice (){};
+    private boolean isMusic (){};
+    private boolean isGames (){};
+    private boolean isReadind (){};
+    private boolean isSports (){};
+    private boolean isShopping (){};
+    public String getName (){};
+    public String getPlace(){};
+    public int getDate(){};
+    public int getQuantity(){};
+    private setTime(new_time){
+        time = new_time;
+    }
+}
