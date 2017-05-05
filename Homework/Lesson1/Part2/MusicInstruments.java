d965736ecfbf90690a38e4944afe79c6ae271baa
Jane Geryak <janegeryak@gmail.com>
2017-05-05 10:46:32 +0300

    class MusicInstrument

---
+++ Homework/Lesson1/Part2/MusicInstruments.java
@@ -0,0 +1,27 @@
+public class MusicInstruments {
+    public String name;
+    public String model;
+    public String group;
+    public int wieght;
+    public int height;
+    public int volume;
+    public int price;
+    public long exploitation_period;
+    public long year_of_manufacture;
+    private int tuned;
+    private int detuned;
+
+    public float getPrice (){};
+    private boolean isString (){};
+    private boolean isWoodwind (){};
+    private boolean isBrass (){};
+    private boolean isPercussion (){};
+    public String getName (){};
+    public String getModel(){};
+    public int getWieght(){};
+    public int getHeight(){};
+    private setExploitationPeriod(exploitation_period){
+        period = new_period;
+    }
+    private setYear(year_of_manufacture){ year = year; }
+}
