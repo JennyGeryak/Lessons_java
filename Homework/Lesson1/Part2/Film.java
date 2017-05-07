7a76ca6890ed8094f26200c861cb0a432d35b29a
Jane Geryak <janegeryak@gmail.com>
2017-05-07 21:26:43 +0300

    class film

---
+++ Homework/Lesson1/Part2/Film.java
@@ -0,0 +1,33 @@
+public class Film {
+    public String name;
+    public int year;
+    public String director;
+    public String producer;
+    public String ganre;
+    public short rate;
+    public String studio;
+    public String country;
+    public short rated;
+    public String [] avards;
+
+    public String getName () {};
+    public setName (String new_name) {
+        name = new_name;
+    };
+    public int getYear () {};
+    public setYear (int new_year) {
+        year = new_year;
+    };
+    public String getDirector () {};
+    public setDirector (String new_director) {
+        director = new_director;
+    };
+    public String getProducer () {};
+    public setProducer (String new_producer) {
+        producer = new_producer;
+    };
+    public String getGanre () {};
+    public setGanre (String new_ganre) {
+        ganre = new_ganre;
+    };
+}
