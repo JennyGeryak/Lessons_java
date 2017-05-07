f7c30f250459cb0084ec237af2232ace17f2311b
Jane Geryak <janegeryak@gmail.com>
2017-05-07 22:50:16 +0300

    class SchoolWorker

---
+++ Homework/Lesson1/Part3/SchoolWorker.java
@@ -0,0 +1,37 @@
+public class SchoolWorker{
+    public String name;
+    public String sex;
+    public int age;
+
+    public work(){};
+    public rest(){};
+}
+
+public class Intern extends SchoolWorker{
+    public boolean isOwnCabinet;
+    public boolean isHasClass;
+    public String[] knowledge;
+
+    public checkNotebooks(){};
+    public waterFlowers(){};
+}
+
+public class Teacher extends Intern{
+    public short expiriense;
+    public String[] skills;
+    public String degree;
+
+    public fillInJournal();
+    public giveHomework();
+}
+
+public class Principal extends Teacher{
+    public String brandOfCar;
+    public boolean barInCabinet;
+    public int secretaryCount;
+
+    public driveCar();
+    public askSomeCoffee();
+    public takeBrabe();
+}
+
