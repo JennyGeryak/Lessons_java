a2c66ac7b6ea9aee35965bb1501d37542625a1e4
Jane Geryak <janegeryak@gmail.com>
2017-05-09 13:55:57 +0300

    getter setter CustomerGoods

--- Homework/Lesson1/Part2/CustomerGoods.java
+++ Homework/Lesson1/Part2/CustomerGoods.java
@@ -10,20 +10,88 @@ public class CustomerGoods {
     private int barcode;
     private String product_id;
 
-    public float getPrice (){};
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getWieght() {
+        return wieght;
+    }
+
+    public void setWieght(int wieght) {
+        this.wieght = wieght;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public long getDate_of_manufacture() {
+        return date_of_manufacture;
+    }
+
+    public void setDate_of_manufacture(long date_of_manufacture) {
+        this.date_of_manufacture = date_of_manufacture;
+    }
+
+    public long getShelf_life() {
+        return shelf_life;
+    }
+
+    public void setShelf_life(long shelf_life) {
+        this.shelf_life = shelf_life;
+    }
+
+    public int getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(int barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(String product_id) {
+        this.product_id = product_id;
+    }
+
     private boolean isFresh (){};
     private boolean isAvailible (){};
     public buy (){};
-    public String getName (){};
-    public String getCategory(){};
-    public int getWieght(){};
-    public int getHeight(){};
-    public int getWidth(){};
-    public int getLength(){};
-    private setName(String new_name){
-        name = new_name;
-    }
-    private setCategory(String new_category){
-        category = new_category;
-    }
+
 }
