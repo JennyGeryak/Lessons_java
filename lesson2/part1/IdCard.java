ab97afe38d7691bd8d0c9ad2f3fab8c5112e5a64
Jane Geryak <janegeryak@gmail.com>
2017-05-18 13:15:29 +0300

    IdCard class imports

--- lesson2/part1/IdCard.java
+++ lesson2/part1/IdCard.java
@@ -1,21 +1,33 @@
 package lesson2.part1;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by geryak on 18.05.2017.
+ * java
  */
 public class IdCard {
     private Date dateExpire;
     private int number;
-    public IdCard(int n){
+    private IdCard(int n, Employee sysEngineer) throws ParseException {
         number = n;
+
+        IdCard card = new IdCard(123, sysEngineer);
+        card.setDateExpire(new SimpleDateFormat("yyyy-MM-dd").parse("2015-12-31"));
+        sysEngineer.setIdCard(card);
+        System.out.println(sysEngineer.getName() +" работает в должности "+ sysEngineer.getPosition());
+        System.out.println("Удостовирение действует до " + new SimpleDateFormat("yyyy-MM-dd").format(sysEngineer.getIdCard().getDateExpire()) );
     }
+
     public void setNumber(int newNumber){
         number = newNumber;
     }
     public int getNumber(){
         return number;
     }
-    public void setDateExpire(Date newDateExpire){
+    private void setDateExpire(Date newDateExpire){
         dateExpire = newDateExpire;
     }
     public Date getDateExpire(){
