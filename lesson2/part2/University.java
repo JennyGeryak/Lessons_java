cd807848dc88924de7b051957f31862bf2a652d5
Jane Geryak <janegeryak@gmail.com>
2017-05-22 16:50:42 +0300

    class University

--- lesson2/part2/University.java
+++ lesson2/part2/University.java
@@ -5,4 +5,32 @@ package lesson2.part2;
  * java
  */
 public class University {
+        protected String name;
+        protected String address;
+        protected String student;
+        protected String faculty;
+        protected int phone;
+
+    public String addStudent() {
+        return student;
+    };
+    public String removeStudent () {
+        return student;
+    };
+    public String getStudent() {
+        return student;
+    }
+    public String addFaculty () {
+        return address;
+    };
+    public String removeFaculty () {
+        return faculty;
+    };
+    public String getFaculty() {
+        return faculty;
+    }
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
 }
