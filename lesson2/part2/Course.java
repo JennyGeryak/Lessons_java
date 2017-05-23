9829065d80c3d5c3cd43a55643e05458248567fa
Jane Geryak <janegeryak@gmail.com>
2017-05-23 10:07:36 +0300

    classes Course and Student

--- lesson2/part2/Course.java
+++ lesson2/part2/Course.java
@@ -4,5 +4,22 @@ package lesson2.part2;
  * Created by geryak on 22.05.2017.
  * java
  */
-public class Course {
+public class Course extends Faculty {
+    private String name;
+    private int courseID;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
 }
--- lesson2/part2/Student.java
+++ lesson2/part2/Student.java
@@ -1,8 +1,22 @@
 package lesson2.part2;
 
+
 /**
  * Created by geryak on 22.05.2017.
  * java
  */
-public class Student {
+public class Student extends Course{
+    private String name;
+    private int StudentID;
+
+    public void study(){
+
+    };
+    public void doHomework(){
+
+    };
+    public void getKnowledge(){
+
+    };
+
 }
