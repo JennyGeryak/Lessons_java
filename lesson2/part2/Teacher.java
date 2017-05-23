71a7ef0b9bc06278838c2c4d2ccf4528b3515977
Jane Geryak <janegeryak@gmail.com>
2017-05-23 10:42:06 +0300

    class Teacher

--- lesson2/part2/Teacher.java
+++ lesson2/part2/Teacher.java
@@ -4,5 +4,24 @@ package lesson2.part2;
  * Created by geryak on 22.05.2017.
  * java
  */
-public class Teacher {
+public class Teacher extends Course {
+    private String name;
+    private int age;
+    private String subject;
+    private String lesson;
+    private int workHour;
+
+    public void fillInJournal(){
+
+    };
+    public void giveHomework(){
+
+    };
+    public void checkNotebooks(){
+
+    };
+    public void giveLesson(){
+
+    };
+
 }
