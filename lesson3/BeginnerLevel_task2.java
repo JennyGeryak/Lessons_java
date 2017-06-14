775f20ed2c964f9df5d96874480062b533dced06
Jane Geryak <janegeryak@gmail.com>
2017-06-14 16:22:11 +0300

    BeginnerLevel_task2

--- lesson3/BeginnerLevel_task2.java
+++ lesson3/BeginnerLevel_task2.java
@@ -2,6 +2,16 @@ package lesson3;
 
 /**
  * Created by geryak on 14.06.2017.
+ * java
  */
 public class BeginnerLevel_task2 {
+    public static void main (String[]args) {
+        int x = 10;
+        int y = 10;
+        int sum = x + y;
+
+        if (sum == 10 || x == 10 || y == 10) {
+            System.out.println("True");
+        }
+    }
 }
