de93af7aed42faa2329255bf3ccf19f907f97be7
Jane Geryak <janegeryak@gmail.com>
2017-06-14 16:40:12 +0300

    BeginnerLevel_task3

--- lesson3/BeginnerLevel_task3.java
+++ lesson3/BeginnerLevel_task3.java
@@ -2,6 +2,13 @@ package lesson3;
 
 /**
  * Created by geryak on 14.06.2017.
+ * java
  */
 public class BeginnerLevel_task3 {
-}
+    public static void main(String[] args) {
+        int n = 35;
+            if (n % 3 == 0 || n % 5 == 0)
+                System.out.println("True");
+        }
+    }
+
